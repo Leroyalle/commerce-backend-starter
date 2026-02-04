10281e384bb1c6264197ff529248f98cabc583db
Leroyalle <nikolajmelonov@yandex.ru>
2026-02-04 13:36:25 +0300

    feat(cart): add delete and put routes

--- src/modules/cart/cart.router.ts
+++ src/modules/cart/cart.router.ts
@@ -27,5 +27,19 @@ export function createCartRouter(deps: Deps): Hono {
     return c.json(data);
   });
 
+  router.delete('/items/:id', deps.accessAuthMiddleware, async c => {
+    const userId = c.get('userId');
+    const cartItemId = c.req.param('id');
+    const data = await deps.commands.removeItem(userId, cartItemId);
+    return c.json(data);
+  });
+
+  router.put('/items/:id', deps.accessAuthMiddleware, async c => {
+    const userId = c.get('userId');
+    const cartItemId = c.req.param('id');
+    const data = await deps.commands.decrementItem(userId, cartItemId);
+    return c.json(data);
+  });
+
   return router;
 }
