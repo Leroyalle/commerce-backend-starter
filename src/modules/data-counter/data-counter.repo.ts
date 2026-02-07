8deb4483a24f7062a5d54f388be58f6b35152ab9
Leroyalle <nikolajmelonov@yandex.ru>
2026-02-07 20:08:00 +0300

    feat(data-counter): add schema and repository

---
+++ src/modules/data-counter/data-counter.repo.ts
@@ -0,0 +1,24 @@
+import { eq, sql } from 'drizzle-orm';
+import { DrizzleD1Database } from 'drizzle-orm/d1';
+
+import { dataCounterSchema } from '@/shared/infrastructure/db/schema/data-counter.schema';
+
+interface Deps {
+  db: DrizzleD1Database;
+}
+
+interface IDataCounterRepository {
+  updateCount: (type: 'increment' | 'decrement', tableName: string, tx: DrizzleD1Database) => void;
+}
+
+export class DataCounterRepository implements IDataCounterRepository {
+  constructor(private readonly deps: Deps) {}
+
+  public async updateCount(type: 'increment' | 'decrement', tableName: string, tx = this.deps.db) {
+    const value = type === 'increment' ? 1 : -1;
+    await tx
+      .update(dataCounterSchema)
+      .set({ totalCount: sql`${dataCounterSchema.totalCount} + ${value}` })
+      .where(eq(dataCounterSchema.tableName, tableName));
+  }
+}
--- src/shared/infrastructure/db/schema/data-counter.schema.ts
+++ src/shared/infrastructure/db/schema/data-counter.schema.ts
@@ -1,6 +1,6 @@
 import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
 
 export const dataCounterSchema = pgTable('data-counter', {
-  name: varchar().notNull(),
-  counter: integer().notNull(),
+  tableName: varchar().notNull(),
+  totalCount: integer().notNull(),
 });
