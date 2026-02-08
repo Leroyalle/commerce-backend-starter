fd6b5e5d6985a8371f4281483aae67253dcac45d
Leroyalle <nikolajmelonov@yandex.ru>
2026-02-09 00:46:53 +0300

    feat(mailer): add service with send handler

---
+++ src/modules/mailer/mailer.service.ts
@@ -0,0 +1,19 @@
+import { Transporter } from 'nodemailer';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+
+import { ISendEmailPayload } from '@/shared/types/send-email-payload.type';
+
+export class MailerService {
+  constructor(
+    private readonly client: Transporter<SMTPTransport.SentMessageInfo, SMTPTransport.Options>,
+  ) {}
+
+  public async send(payload: ISendEmailPayload) {
+    await this.client.sendMail({
+      from: 'RESTORE',
+      to: payload.to,
+      subject: payload.subject,
+      text: payload.text,
+    });
+  }
+}
