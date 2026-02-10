553c0b89020b1a6e046d3aad7e8e979728211a4a
Leroyalle <nikolajmelonov@yandex.ru>
2026-02-10 22:31:37 +0300

    feat(exceptions): add universal not found and already exists exceptions

--- src/shared/exceptions/exceptions.ts
+++ src/shared/exceptions/exceptions.ts
@@ -6,22 +6,31 @@ export abstract class DomainException extends Error {
   }
 }
 
+export class NotFoundException extends DomainException {
+  constructor(entity: string) {
+    super(`${entity} не найден`);
+  }
+}
+
+export class AlreadyExistsException extends DomainException {
+  constructor(entity: string) {
+    super(`${entity} уже существует`);
+  }
+}
+
 export abstract class AuthException extends DomainException {}
-export class UserNotFoundException extends AuthException {}
-export class UserAlreadyExistsException extends AuthException {}
+export class UserNotVerifiedException extends AuthException {}
 export class InvalidPasswordException extends AuthException {}
+export class SamePasswordException extends AuthException {}
 export class TokenExpiredException extends AuthException {}
 export class InvalidTokenException extends AuthException {}
 export class RoleForbiddenException extends AuthException {}
 
 export abstract class ProductException extends DomainException {}
-export class ProductNotFoundException extends ProductException {}
-export class CategoryNotFoundException extends ProductException {}
 export class ProductOutOfStockException extends ProductException {}
 export class InsufficientStockException extends ProductException {}
 
 export abstract class OrderException extends DomainException {}
-export class OrderNotFoundException extends OrderException {}
 export class CartIsEmptyException extends OrderException {}
 
 export class InternalServerException extends DomainException {}
