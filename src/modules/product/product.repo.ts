1ccb34eafe8d8b54e0e9fffa05b459777404c998
Leroyalle <nikolajmelonov@yandex.ru>
2026-02-17 02:21:04 +0300

    feat(product): bind categories to products on creation

--- src/modules/product/product.repo.ts
+++ src/modules/product/product.repo.ts
@@ -2,18 +2,38 @@ import { count, desc, eq } from 'drizzle-orm';
 
 import { db } from '@/shared/infrastructure/db/client';
 import { Product, productSchema } from '@/shared/infrastructure/db/schema/product.schema';
+import { productsToCategories } from '@/shared/infrastructure/db/schema/products-to-categories.schema';
 import { IPaginationResult } from '@/shared/types/pagination-result.type';
 import { IPagination } from '@/shared/types/pagination.type';
 
 export interface IProductRepository {
-  create(data: { name: string; price: number; aliases: string[] }): Promise<Product>;
+  create(data: {
+    name: string;
+    price: number;
+    aliases: string[];
+    categories: string[];
+  }): Promise<Product>;
   findAll(pagination?: IPagination): Promise<IPaginationResult<Product>>;
   findById(id: string): Promise<Product>;
 }
 
 export class ProductRepo implements IProductRepository {
-  public async create(data: { name: string; price: number }): Promise<Product> {
-    return (await db.insert(productSchema).values(data).returning())[0];
+  public async create(data: {
+    name: string;
+    price: number;
+    aliases: string[];
+    categories: string[];
+  }): Promise<Product> {
+    const [product] = await db.insert(productSchema).values(data).returning();
+
+    for (const categoryId of data.categories) {
+      await db.insert(productsToCategories).values({
+        productId: product.id,
+        categoryId: categoryId,
+      });
+    }
+
+    return product;
   }
 
   public async findAll(pagination?: IPagination): Promise<IPaginationResult<Product>> {
