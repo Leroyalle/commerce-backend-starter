a0a2561855c98d984a6307d47894e73bda3d96ed
Leroyalle <nikolajmelonov@yandex.ru>
2026-02-17 01:04:18 +0300

    feat(product): add details in schema

--- src/shared/infrastructure/db/schema/product.schema.ts
+++ src/shared/infrastructure/db/schema/product.schema.ts
@@ -1,5 +1,5 @@
 import { InferSelectModel, relations } from 'drizzle-orm';
-import { integer, pgTable, text, uuid } from 'drizzle-orm/pg-core';
+import { integer, jsonb, pgTable, text, uuid } from 'drizzle-orm/pg-core';
 
 import { cartItemSchema } from './cart-item.schema';
 import { pgTimestamp } from './timestamp';
@@ -8,6 +8,7 @@ export const productSchema = pgTable('products', {
   id: uuid().defaultRandom().primaryKey(),
   name: text().notNull(),
   price: integer().notNull(),
+  details: jsonb().$type<Record<string, unknown>>().default({}),
   aliases: text().array().$type<string[]>().default([]),
   ...pgTimestamp,
 });
