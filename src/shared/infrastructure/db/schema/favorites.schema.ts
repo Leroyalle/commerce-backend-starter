19c3b00d35530cc6795bf0b76ac0b90fc5c05a44
Leroyalle <nikolajmelonov@yandex.ru>
2026-02-23 18:51:37 +0300

    feat(favorites): change pk from id to composite pk

--- src/shared/infrastructure/db/schema/favorites.schema.ts
+++ src/shared/infrastructure/db/schema/favorites.schema.ts
@@ -1,23 +1,29 @@
 import { type InferSelectModel, relations } from 'drizzle-orm';
-import { pgTable, uuid } from 'drizzle-orm/pg-core';
+import { pgTable, primaryKey, uuid } from 'drizzle-orm/pg-core';
 
 import { productSchema } from './product.schema';
 import { pgTimestamp } from './timestamp';
 import { userSchema } from './user.schema';
 
-export const favoritesSchema = pgTable('favorites', {
-  id: uuid().defaultRandom().primaryKey(),
-  productId: uuid().references(() => productSchema.id),
-  userId: uuid().references(() => userSchema.id),
-  ...pgTimestamp,
-});
+export const favoritesSchema = pgTable(
+  'favorites',
+  {
+    productId: uuid().references(() => productSchema.id),
+    userId: uuid().references(() => userSchema.id),
+    ...pgTimestamp,
+  },
+  table => ({
+    pk: primaryKey({
+      columns: [table.userId, table.productId],
+    }),
+  }),
+);
 
 export const favoritesRelations = relations(favoritesSchema, ({ one }) => ({
   product: one(productSchema, {
     fields: [favoritesSchema.productId],
     references: [productSchema.id],
   }),
-
   user: one(userSchema, {
     fields: [favoritesSchema.userId],
     references: [userSchema.id],
