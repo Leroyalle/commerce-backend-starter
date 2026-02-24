6a7495260032b08bd1de3d0e6e5c8c4d0e7d9778
Leroyalle <nikolajmelonov@yandex.ru>
2026-02-24 12:56:27 +0300

    feat(favorites): add commands

---
+++ src/modules/favorites/favorites.commands.ts
@@ -0,0 +1,24 @@
+import type { Favorite } from '@/shared/infrastructure/db/schema/favorite.schema';
+
+import type { IFavoritesRepository } from './favorites.repo';
+
+export interface IFavoritesCommands {
+  add(data: Omit<Favorite, 'createdAt' | 'updatedAt'>): Promise<Favorite>;
+  remove(data: Omit<Favorite, 'createdAt' | 'updatedAt'>): Promise<Favorite>;
+}
+
+interface Deps {
+  favoritesRepo: IFavoritesRepository;
+}
+
+export class FavoritesCommands implements IFavoritesCommands {
+  constructor(private readonly deps: Deps) {}
+
+  public async add(data: Omit<Favorite, 'createdAt' | 'updatedAt'>): Promise<Favorite> {
+    return this.deps.favoritesRepo.create(data);
+  }
+
+  public async remove(data: { userId: string; productId: string }) {
+    return this.deps.favoritesRepo.remove(data);
+  }
+}
